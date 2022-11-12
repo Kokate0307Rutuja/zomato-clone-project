d9d6170fc51cc5713f9aaff4abe420ee34a7c542
Kokate0307Rutuja <rutujakokate2000@gmail>
2022-11-12 09:20:09 +0530

    complete user authication

--- server/index.js
+++ server/index.js
@@ -1,24 +1,35 @@
-import express from 'express';
-import dotenv from 'dotenv';
-//database connection
-import ConnectDB from './database/connection';
-dotenv.config();
-const zomato = express();
-zomato.use(express.json());
-zomato.get("/",(req,res) => {
-    res.json({
-        message:"Server is running",
-    });
-});
-const PORT = 4000;
-zomato.listen(PORT,() =>{
-    ConnectDB()
-    .then(() => {
-      console.log("Server is running!!!");
-    })
-   .catch((error) => {
-        console.log("Server is running,but database connection failed....");
-        console.log("error");
-   });
+import express from "express";
+import dotenv from "dotenv";
 
+// Database connection
+import ConnectDB from "./database/connection";
+
+import Auth from "./api/auth";
+
+dotenv.config();
+
+const zomato = express();
+
+zomato.use(express.json());
+
+zomato.get("/", (req, res) => {
+  res.json({
+    message: "Server is running",
+  });
+});
+
+// /auth/signup
+zomato.use("/auth", Auth);
+
+const PORT = 4000;
+
+zomato.listen(PORT, () => {
+  ConnectDB()
+    .then(() => {
+      console.log("Server is running !!!");
+    })
+    .catch((error) => {
+      console.log("Server is running, but database connection failed...");
+      console.log(error);
+    });
 });
