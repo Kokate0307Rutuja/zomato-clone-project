fbf369d915862a6459f4e4813d307b1db342ace7
Kokate0307Rutuja <rutujakokate2000@gmail>
2022-11-17 08:06:43 +0530

    complete server

--- server/config/google.config.js
+++ server/config/google.config.js
@@ -10,7 +10,7 @@ export default (passport) => {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: `${process.env.REACT_APP_CLIENT_URL}auth/google/callback`,
+        callbackURL: "http://localhost:4000/auth/google/callback",
       },
 
       async (accessToken, refreshToken, profile, done) => {
